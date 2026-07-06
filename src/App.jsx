70f077522e5a5c40779ce4a85ecb40eb70433698
전지후 <jeonjihoo@jeonjihuui-MacBookAir.local>
2026-07-06 14:12:26 +0900

    파일삭제

--- src/App.jsx
+++ src/App.jsx
@@ -6,7 +6,7 @@ import MovieCard from "./MovieCard.jsx";
 import { useEffect, useState } from "react";
 import SearchPage from "./component/SearchPage.jsx";
 import MovieDetail from "./MovieDetail.jsx";
-import LogIn from "./pages/Login.jsx";
+import LogIn from "./pages/LogIn.jsx";
 import About from "./component/About.jsx";
 
 function MovieList() {
